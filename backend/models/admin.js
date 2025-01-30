d82197a781a5d9a5cd03d76efea8146c5bcaea0a
jayesh pj <jayeshpj343@gmail.com>
2025-01-30 14:22:52 +0530

    Initial commit

--- backend/models/admin.js
+++ backend/models/admin.js
@@ -7,18 +7,5 @@ const AdminSchema = new Schema({
   password: { type: String, required: true },
 });
 
-// Hash password before saving
-AdminSchema.pre('save', async function(next) {
-  if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
-
-// Compare password with hashed password
-AdminSchema.methods.comparePassword = async function(password) {
-  return bcrypt.compare(password, this.password);
-};
-
 const Admin = mongoose.model('AdminDetails', AdminSchema);
 module.exports = Admin;
